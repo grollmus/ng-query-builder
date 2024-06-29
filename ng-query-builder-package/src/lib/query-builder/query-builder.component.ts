fce757f02f2f9ce03a2d130f31613272e9cb3a51
Daniel Weiß <daniel.weiss@grollmus.de>
2024-06-29 18:34:28 +0200

    next migration, builds.

--- ng-query-builder-package/src/lib/query-builder/query-builder.component.ts
+++ ng-query-builder-package/src/lib/query-builder/query-builder.component.ts
@@ -14,12 +14,15 @@ import {
 } from '@angular/core';
 import {
   ArrowIconContext,
+  ButtonGroupContext,
+  Field,
   QueryBuilderClassName,
   QueryBuilderConfig,
   RuleSet,
 } from './query-builder.interfaces';
 import { QueryArrowIconDirective } from './query-arrow-icon.directive';
 import { cssMap } from './css.map';
+import { QueryButtonGroupDirective } from './query-button-group.directive';
 
 export const CONTROL_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
@@ -41,50 +44,52 @@ export const VALIDATOR: any = {
 })
 export class QueryBuilderComponent {
   #cssMap = cssMap;
-  // public fields!: Field[];
+  fields!: Field[];
   // public filterFields!: Field[];
   // public entities!: Entity[];
   defaultClassNames: { [key in QueryBuilderClassName]: string };
-  // public defaultOperatorMap: { [key: string]: string[] } = {
-  //   string: ['=', '!=', 'contains', 'like'],
-  //   number: ['=', '!=', '>', '>=', '<', '<='],
-  //   time: ['=', '!=', '>', '>=', '<', '<='],
-  //   date: ['=', '!=', '>', '>=', '<', '<='],
-  //   category: ['=', '!=', 'in', 'not in'],
-  //   boolean: ['=']
-  // };
+  defaultOperatorMap: { [key: string]: string[] } = {
+    string: ['=', '!=', 'contains', 'like'],
+    number: ['=', '!=', '>', '>=', '<', '<='],
+    time: ['=', '!=', '>', '>=', '<', '<='],
+    date: ['=', '!=', '>', '>=', '<', '<='],
+    category: ['=', '!=', 'in', 'not in'],
+    boolean: ['='],
+  };
   disabled = input(false);
   data = input<RuleSet>({ condition: 'and', rules: [], collapsed: false });
   _data = signal<RuleSet>({ condition: 'and', rules: [], collapsed: false });
 
   // // For ControlValueAccessor interface
-  // public onChangeCallback!: () => void;
-  // public onTouchedCallback!: () => any;
+  onChangeCallback!: () => void;
+  onTouchedCallback!: () => any;
 
-  // @Input() allowRuleset = true;
+  @Input() allowRuleset = true;
   allowCollapse = input<boolean>(false);
   // @Input() emptyMessage = 'A ruleset cannot be empty. Please add a rule or remove it all together.';
   @Input() classNames!: { [key in QueryBuilderClassName]: string };
-  // @Input() operatorMap!: { [key: string]: string[] };
-  // @Input() parentValue!: RuleSet;
+  @Input() operatorMap!: { [key: string]: string[] };
+  @Input() parentValue!: RuleSet;
   config = input<QueryBuilderConfig>({ fields: {} });
+  _config: QueryBuilderConfig = { fields: {} };
   parentArrowIconTemplate = input<QueryArrowIconDirective>();
   // @Input() parentInputTemplates!: QueryList<QueryInputDirective>;
   // @Input() parentOperatorTemplate!: QueryOperatorDirective;
   // @Input() parentFieldTemplate!: QueryFieldDirective;
   // @Input() parentEntityTemplate!: QueryEntityDirective;
   // @Input() parentSwitchGroupTemplate!: QuerySwitchGroupDirective;
-  // @Input() parentButtonGroupTemplate!: QueryButtonGroupDirective;
+  @Input() parentButtonGroupTemplate!: QueryButtonGroupDirective;
   // @Input() parentRemoveButtonTemplate!: QueryRemoveButtonDirective;
   // @Input() parentEmptyWarningTemplate!: QueryEmptyWarningDirective;
-  // @Input() parentChangeCallback!: () => void;
-  // @Input() parentTouchedCallback!: () => void;
+  @Input() parentChangeCallback!: () => void;
+  @Input() parentTouchedCallback!: () => void;
   // @Input() persistValueOnFieldChange = false;
 
   @ViewChild('treeContainer', { static: true })
   treeContainer!: ElementRef;
 
-  // @ContentChild(QueryButtonGroupDirective) buttonGroupTemplate!: QueryButtonGroupDirective;
+  @ContentChild(QueryButtonGroupDirective)
+  buttonGroupTemplate!: QueryButtonGroupDirective;
   // @ContentChild(QuerySwitchGroupDirective) switchGroupTemplate!: QuerySwitchGroupDirective;
   // @ContentChild(QueryFieldDirective) fieldTemplate!: QueryFieldDirective;
   // @ContentChild(QueryEntityDirective) entityTemplate!: QueryEntityDirective;
@@ -99,14 +104,14 @@ export class QueryBuilderComponent {
   //   'string', 'number', 'time', 'date', 'category', 'boolean', 'multiselect'];
   // private defaultPersistValueTypes: string[] = [
   //   'string', 'number', 'time', 'date', 'boolean'];
-  // private defaultEmptyList: any[] = [];
-  // private operatorsCache!: { [key: string]: string[] };
+  private defaultEmptyList: any[] = [];
+  private operatorsCache!: { [key: string]: string[] };
   // private inputContextCache = new Map<Rule, InputContext>();
   // private operatorContextCache = new Map<Rule, OperatorContext>();
   // private fieldContextCache = new Map<Rule, FieldContext>();
   // private entityContextCache = new Map<Rule, EntityContext>();
   // private removeButtonContextCache = new Map<Rule, RemoveButtonContext>();
-  // private buttonGroupContext!: ButtonGroupContext;
+  private buttonGroupContext!: ButtonGroupContext;
 
   constructor(private changeDetectorRef: ChangeDetectorRef) {
     this.defaultClassNames = this.#cssMap;
@@ -114,6 +119,7 @@ export class QueryBuilderComponent {
     effect(
       () => {
         this._data.update(() => this.data());
+        this._config = this.config();
       },
       { allowSignalWrites: true }
     );
@@ -229,39 +235,43 @@ export class QueryBuilderComponent {
   //   return templates.find((item) => item.queryInputType === type);
   // }
 
-  // getOperators(field: string): string[] {
-  //   if (this.operatorsCache[field]) {
-  //     return this.operatorsCache[field];
-  //   }
-  //   let operators = this.defaultEmptyList;
-  //   const fieldObject = this.config.fields[field];
+  getOperators(field: string): string[] {
+    if (this.operatorsCache[field]) {
+      return this.operatorsCache[field];
+    }
+    let operators = this.defaultEmptyList;
+    const fieldObject = this._config.fields[field];
 
-  //   if (this.config.getOperators) {
-  //     return this.config.getOperators(field, fieldObject);
-  //   }
+    if (this._config.getOperators) {
+      return this._config.getOperators(field, fieldObject);
+    }
 
-  //   const type = fieldObject.type;
+    const type = fieldObject.type;
 
-  //   if (fieldObject && fieldObject.operators) {
-  //     operators = fieldObject.operators;
-  //   } else if (type) {
-  //     operators = (this.operatorMap && this.operatorMap[type]) || this.defaultOperatorMap[type] || this.defaultEmptyList;
-  //     if (operators.length === 0) {
-  //       console.warn(
-  //         `No operators found for field '${field}' with type ${fieldObject.type}. ` +
-  //         `Please define an 'operators' property on the field or use the 'operatorMap' binding to fix this.`);
-  //     }
-  //     if (fieldObject.nullable) {
-  //       operators = operators.concat(['is null', 'is not null']);
-  //     }
-  //   } else {
-  //     console.warn(`No 'type' property found on field: '${field}'`);
-  //   }
+    if (fieldObject && fieldObject.operators) {
+      operators = fieldObject.operators;
+    } else if (type) {
+      operators =
+        (this.operatorMap && this.operatorMap[type]) ||
+        this.defaultOperatorMap[type] ||
+        this.defaultEmptyList;
+      if (operators.length === 0) {
+        console.warn(
+          `No operators found for field '${field}' with type ${fieldObject.type}. ` +
+            `Please define an 'operators' property on the field or use the 'operatorMap' binding to fix this.`
+        );
+      }
+      if (fieldObject.nullable) {
+        operators = operators.concat(['is null', 'is not null']);
+      }
+    } else {
+      console.warn(`No 'type' property found on field: '${field}'`);
+    }
 
-  //   // Cache reference to array object, so it won't be computed next time and trigger a rerender.
-  //   this.operatorsCache[field] = operators;
-  //   return operators;
-  // }
+    // Cache reference to array object, so it won't be computed next time and trigger a rerender.
+    this.operatorsCache[field] = operators;
+    return operators;
+  }
 
   //   getFields(entity: string): Field[] {
   //     if (this.entities && entity) {
@@ -335,42 +345,44 @@ export class QueryBuilderComponent {
   //   }
   // }
 
-  // getDefaultOperator(field: Field) {
-  //   if (field && field.defaultOperator !== undefined) {
-  //     return this.getDefaultValue(field.defaultOperator);
-  //   } else {
-  //     const operators = this.getOperators(field.value!);
-  //     if (operators && operators.length) {
-  //       return operators[0];
-  //     } else {
-  //       console.warn(`No operators found for field '${field.value}'. ` +
-  //         `A 'defaultOperator' is also not specified on the field config. Operator value will default to null.`);
-  //       return null;
-  //     }
-  //   }
-  // }
+  getDefaultOperator(field: Field) {
+    if (field && field.defaultOperator !== undefined) {
+      return this.getDefaultValue(field.defaultOperator);
+    } else {
+      const operators = this.getOperators(field.value!);
+      if (operators && operators.length) {
+        return operators[0];
+      } else {
+        console.warn(
+          `No operators found for field '${field.value}'. ` +
+            `A 'defaultOperator' is also not specified on the field config. Operator value will default to null.`
+        );
+        return null;
+      }
+    }
+  }
 
-  // addRule(parent?: RuleSet): void {
-  //   if (this.disabled) {
-  //     return;
-  //   }
+  addRule(parent?: RuleSet): void {
+    if (this.disabled()) return;
 
-  //   parent = parent || this.data;
-  //   if (this.config.addRule) {
-  //     this.config.addRule(parent);
-  //   } else {
-  //     const field = this.fields[0];
-  //     parent.rules = parent.rules.concat([{
-  //       field: field.value!,
-  //       operator: this.getDefaultOperator(field),
-  //       value: this.getDefaultValue(field.defaultValue),
-  //       entity: field.entity
-  //     }]);
-  //   }
+    parent = parent || this._data();
+    if (this._config && 'addRule' in this._config) {
+      this._config.addRule!(parent); // trust me bro
+    } else {
+      const field = this.fields[0];
+      parent.rules = parent.rules.concat([
+        {
+          field: field.value!,
+          operator: this.getDefaultOperator(field),
+          value: this.getDefaultValue(field.defaultValue),
+          entity: field.entity,
+        },
+      ]);
+    }
 
-  //   this.handleTouched();
-  //   this.handleDataChange();
-  // }
+    this.handleTouched();
+    this.handleDataChange();
+  }
 
   // removeRule(rule: Rule, parent?: RuleSet): void {
   //   if (this.disabled) {
@@ -393,42 +405,44 @@ export class QueryBuilderComponent {
   //   this.handleDataChange();
   // }
 
-  // addRuleSet(parent?: RuleSet): void {
-  //   if (this.disabled) {
-  //     return;
-  //   }
+  addRuleSet(parent?: RuleSet): void {
+    if (this.disabled()) {
+      return;
+    }
 
-  //   parent = parent || this.data;
-  //   if (this.config.addRuleSet) {
-  //     this.config.addRuleSet(parent);
-  //   } else {
-  //     parent.rules = parent.rules.concat([{ condition: 'and', rules: [] }]);
-  //   }
+    parent = parent || this._data();
+    if (this._config.addRuleSet) {
+      this._config.addRuleSet(parent);
+    } else {
+      parent.rules = parent.rules.concat([
+        { condition: 'and', rules: [], collapsed: this._data().collapsed },
+      ]);
+    }
 
-  //   this.handleTouched();
-  //   this.handleDataChange();
-  // }
+    this.handleTouched();
+    this.handleDataChange();
+  }
 
-  // removeRuleSet(ruleset?: RuleSet, parent?: RuleSet): void {
-  //   if (this.disabled) {
-  //     return;
-  //   }
+  removeRuleSet(ruleset?: RuleSet, parent?: RuleSet): void {
+    if (this.disabled()) {
+      return;
+    }
 
-  //   ruleset = ruleset || this.data;
-  //   parent = parent || this.parentValue;
-  //   if (this.config.removeRuleSet) {
-  //     this.config.removeRuleSet(ruleset, parent);
-  //   } else {
-  //     parent.rules = parent.rules.filter((r) => r !== ruleset);
-  //   }
+    ruleset = ruleset! || this.data!;
+    parent = parent || this.parentValue;
+    if (this._config.removeRuleSet) {
+      this._config.removeRuleSet(ruleset, parent);
+    } else {
+      parent.rules = parent.rules.filter((r) => r !== ruleset);
+    }
 
-  //   this.handleTouched();
-  //   this.handleDataChange();
-  // }
+    this.handleTouched();
+    this.handleDataChange();
+  }
 
-  // transitionEnd(e: Event): void {
-  //   this.treeContainer.nativeElement.style.maxHeight = null;
-  // }
+  transitionEnd(e: Event): void {
+    this.treeContainer.nativeElement.style.maxHeight = null;
+  }
 
   toggleCollapse(): void {
     // this.computedTreeContainerHeight();
@@ -552,14 +566,14 @@ export class QueryBuilderComponent {
   //   }
   // }
 
-  // getDefaultValue(defaultValue: any): any {
-  //   switch (typeof defaultValue) {
-  //     case 'function':
-  //       return defaultValue();
-  //     default:
-  //       return defaultValue;
-  //   }
-  // }
+  getDefaultValue(defaultValue: any): any {
+    switch (typeof defaultValue) {
+      case 'function':
+        return defaultValue();
+      default:
+        return defaultValue;
+    }
+  }
 
   // getOperatorTemplate(): TemplateRef<any> {
   //   const t = this.parentOperatorTemplate || this.operatorTemplate;
@@ -581,10 +595,10 @@ export class QueryBuilderComponent {
     return (t ? t.template : null)!;
   }
 
-  // getButtonGroupTemplate(): TemplateRef<any> {
-  //   const t = this.parentButtonGroupTemplate || this.buttonGroupTemplate;
-  //   return (t ? t.template : null)!;
-  // }
+  getButtonGroupTemplate(): TemplateRef<any> {
+    const t = this.parentButtonGroupTemplate || this.buttonGroupTemplate;
+    return (t ? t.template : null)!;
+  }
 
   // getSwitchGroupTemplate(): TemplateRef<any> | null {
   //   const t = this.parentSwitchGroupTemplate || this.switchGroupTemplate;
@@ -610,18 +624,25 @@ export class QueryBuilderComponent {
   //   return cls!;
   // }
 
-  // getButtonGroupContext(): ButtonGroupContext {
-  //   if (!this.buttonGroupContext) {
-  //     this.buttonGroupContext = {
-  //       addRule: this.addRule.bind(this),
-  //       addRuleSet: this.allowRuleset && this.addRuleSet.bind(this),
-  //       removeRuleSet: this.allowRuleset && this.parentValue && this.removeRuleSet.bind(this),
-  //       getDisabledState: this.getDisabledState,
-  //       $implicit: this.data
-  //     };
-  //   }
-  //   return this.buttonGroupContext;
-  // }
+  getButtonGroupContext(): ButtonGroupContext {
+    if (!this.buttonGroupContext) {
+      this.buttonGroupContext = {
+        addRule: this.addRule.bind(this),
+        addRuleSet:
+          (this.allowRuleset && this.addRuleSet.bind(this)) || (() => null),
+        removeRuleSet: () => {
+          return (
+            this.allowRuleset &&
+            this.parentValue &&
+            this.removeRuleSet.bind(this)
+          );
+        },
+        getDisabledState: this.getDisabledState,
+        $implicit: this._data(),
+      };
+    }
+    return this.buttonGroupContext;
+  }
 
   // getRemoveButtonContext(rule: Rule): RemoveButtonContext {
   //   if (!this.removeButtonContextCache.has(rule)) {
@@ -769,22 +790,22 @@ export class QueryBuilderComponent {
   //   }
   // }
 
-  // private handleDataChange(): void {
-  //   this.changeDetectorRef.markForCheck();
-  //   if (this.onChangeCallback) {
-  //     this.onChangeCallback();
-  //   }
-  //   if (this.parentChangeCallback) {
-  //     this.parentChangeCallback();
-  //   }
-  // }
+  private handleDataChange(): void {
+    this.changeDetectorRef.markForCheck();
+    if (this.onChangeCallback) {
+      this.onChangeCallback();
+    }
+    if (this.parentChangeCallback) {
+      this.parentChangeCallback();
+    }
+  }
 
-  // private handleTouched(): void {
-  //   if (this.onTouchedCallback) {
-  //     this.onTouchedCallback();
-  //   }
-  //   if (this.parentTouchedCallback) {
-  //     this.parentTouchedCallback();
-  //   }
-  // }
+  private handleTouched(): void {
+    if (this.onTouchedCallback) {
+      this.onTouchedCallback();
+    }
+    if (this.parentTouchedCallback) {
+      this.parentTouchedCallback();
+    }
+  }
 }
